61bcb857995ad49e257710374931b37160a93e6c
Reddlice <2396547854@qq.com>
2026-04-26 20:40:41 +0800

    feature: 第一次提交

---
+++ src/main/java/org/reddlice/chatwith/ChatWithApplication.java
@@ -0,0 +1,13 @@
+package org.reddlice.chatwith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatWithApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatWithApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/reddlice/chatwith/ChatWithApplicationTests.java
@@ -0,0 +1,13 @@
+package org.reddlice.chatwith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatWithApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
